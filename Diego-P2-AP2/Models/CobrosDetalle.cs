6308cc8ac564192d9202b8fbb66ea8aec96e7a3a
Diego Valerio <67572551+DiegoJVD@users.noreply.github.com>
2021-06-30 21:29:56 -0400

    Cambiando Modelo de CobrosDetalle

--- Diego-P2-AP2/Models/CobrosDetalle.cs
+++ Diego-P2-AP2/Models/CobrosDetalle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,19 +9,16 @@ namespace Diego_P2_AP2.Models
 {
     public class CobrosDetalle
     {
+        [Key]
         public int id { get; set; }
 
-        public DateTime Fecha { get; set; }
+        public Cobros cobro { get; set; }
 
-        public decimal Monto { get; set; }
+        public Ventas venta { get; set; }
 
-        public int CobroId { get; set; }
-
-        public decimal Balance { get; set; }
+        public int ventaId { get; set; }
 
         public decimal Cobrado { get; set; }
-
-        public bool Pago { get; set; }
 
     }
 }
